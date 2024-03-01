9640195d909789ee4e2b24b9b547c282c55aca36
balogun25517 <balogun25517@cbn.gov.ng>
2024-03-01 12:56:06 +0100

    task 1

--- gitauto.py
+++ gitauto.py
@@ -1,12 +1,21 @@
 import subprocess
 
-# Define a global variable
-counter = 0
+# Function to read and update the counter from a file
+def get_and_update_counter():
+    try:
+        with open('counter.txt', 'r') as file:
+            counter = int(file.read())
+    except FileNotFoundError:
+        # If the file doesn't exist, start with counter value 0
+        counter = 0
 
-# Define a function to increment the counter by one
-def increment_counter():
-    global counter
+    # Increment the counter
     counter += 1
+
+    # Save the updated counter to the file
+    with open('counter.txt', 'w') as file:
+        file.write(str(counter))
+
     return counter
 
 # Function to perform the git operations
@@ -18,9 +27,9 @@ def perform_git_operations(task_number):
 
 # Example command
 def example_command():
-    global counter
     print("This is an example command.")
-    counter_value = increment_counter()
+    counter_value = get_and_update_counter()
+    print("Counter:", counter_value)
     perform_git_operations(counter_value)
 
 # Run the example command and perform git operations
